c6913233746a2aae32b3161fa453dc81f78c1c43
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-23 22:59:11 -0500

    Add application, ports tables

---
+++ scripts/20160123-225901.sql
@@ -0,0 +1,28 @@
+drop table if exists ports;
+drop table if exists applications;
+
+create table applications (
+  id                              text primary key check(util.lower_non_empty_trimmed_string(id))
+);
+
+comment on table applications is '
+  A top level application. The id is the unique identifier of the app
+  and likely maps to a git hub repo.
+';
+
+select audit.setup('public', 'applications');
+
+create table ports (
+  id                              text primary key,
+  application_id                  text not null references applications,
+  number                          bigint not null check(number > 0)
+);
+
+comment on table ports is '
+  Keeps track of ports assigned to a given application,
+  ensuring that no port is reused.
+';
+
+select audit.setup('public', 'ports');
+create index on ports(application_id);
+create unique index on ports(number) where deleted_at is null;
