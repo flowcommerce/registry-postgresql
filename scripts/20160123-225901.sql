612b5a5917ffe4ddda69643e0e2004ad9399942c
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-26 08:49:58 -0500

    Move application type to ports

--- scripts/20160123-225901.sql
+++ scripts/20160123-225901.sql
@@ -15,12 +15,17 @@ select audit.setup('public', 'applications');
 create table ports (
   id                              text primary key,
   application_id                  text not null references applications,
+  type                            text not null check(util.lower_non_empty_trimmed_string(type)),
   number                          bigint not null check(number > 0)
 );
 
 comment on table ports is '
   Keeps track of ports assigned to a given application,
   ensuring that no port is reused.
+';
+
+comment on column ports.type is '
+  The application type running on this port.
 ';
 
 select audit.setup('public', 'ports');
