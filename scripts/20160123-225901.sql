da60b4685576522f1636b99c1a06f2f0acc727f9
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-27 15:47:28 -0500

    Store ports in applications table directly for versioning

--- scripts/20160123-225901.sql
+++ scripts/20160123-225901.sql
@@ -2,7 +2,8 @@ drop table if exists ports;
 drop table if exists applications;
 
 create table applications (
-  id                              text primary key check(util.lower_non_empty_trimmed_string(id))
+  id                              text primary key check(util.lower_non_empty_trimmed_string(id)),
+  ports                           json not null
 );
 
 comment on table applications is '
@@ -10,18 +11,26 @@ comment on table applications is '
   and likely maps to a git hub repo.
 ';
 
+comment on column applications.ports is '
+  A denormalization of the ports table for this
+  application. Introduced to enable versioning of ports for the
+  journal.
+';
+
 select audit.setup('public', 'applications');
 
 create table ports (
   id                              text primary key,
-  application_id                  text not null references applications,
+  application_id                  text not null references applications deferrable initially deferred,
   type                            text not null check(util.lower_non_empty_trimmed_string(type)),
   num                             bigint not null unique check(num > 0)
 );
 
 comment on table ports is '
-  Keeps track of ports assigned to a given application,
-  ensuring that no port is reused.
+  Keeps track of ports assigned to a given application, ensuring that
+  no port is reused. This table is essentially an index to enable
+  faster allocation of unused ports, and also enforces that all of our
+  port allocations are unique.
 ';
 
 comment on column ports.type is '
