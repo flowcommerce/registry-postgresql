631c2fafec900b67ce0e3b133d06b34d245dc2c0
Michael Bryzek <mbryzek@alum.mit.edu>
2016-01-31 22:18:59 -0500

    Include user services as part of bootstrapped assignments

--- scripts/20160130-125408.sql
+++ scripts/20160130-125408.sql
@@ -1,3 +1,5 @@
+-- preassign ports necessary to bootstrap the docker image
+
 insert into applications
 (id, ports, updated_by_user_id)
 values
@@ -18,3 +20,22 @@ insert into ports
 values
 ('prt-20160130-2', 'registry-postgresql', 'play', 6019, 5432, 'usr-20151006-1');
 
+insert into applications
+(id, ports, updated_by_user_id)
+values
+('user', '[{"service":{"id":"play"},"internal":9000,"external":6021}]'::json, 'usr-20151006-1');
+
+insert into applications
+(id, ports, updated_by_user_id)
+values
+('user-postgresql', '[{"service":{"id":"postgresql"},"internal":5432,"external":6029}]', 'usr-20151006-1');
+
+insert into ports
+(id, application_id, service_id, external, internal, updated_by_user_id)
+values
+('prt-20160130-3', 'user', 'play', 6021, 9000, 'usr-20151006-1');
+
+insert into ports
+(id, application_id, service_id, external, internal, updated_by_user_id)
+values
+('prt-20160130-4', 'user-postgresql', 'play', 6029, 5432, 'usr-20151006-1');
