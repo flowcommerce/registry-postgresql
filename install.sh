f6b15141627afe3bc8a79b27d2b7fa941c997126
Michael Bryzek <mbryzek@alum.mit.edu>
2016-02-17 11:05:57 -0500

    Update to latest psql standards

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-psql -U postgres -c 'create database "registry"' postgres
+psql -U postgres -c 'create database registrydb' postgres
 psql -U postgres --no-align --tuples-only -c "SELECT 1 FROM pg_roles WHERE rolname='api'" | grep -q 1 || psql -U postgres -c 'create role api login PASSWORD NULL' postgres
-psql -U postgres -c 'GRANT ALL ON DATABASE "registry" TO api' postgres
-sem-apply --url postgresql://api@localhost/registry
+psql -U postgres -c 'GRANT ALL ON DATABASE registrydb TO api' postgres
+sem-apply --url postgresql://api@localhost/registrydb
