2f2a0ac11a7d72e9519c4300f1614da4c5635737
Nicola Girardi <nicola.girardi@global-e.com>
2024-02-01 17:45:13 +0000

    FDN-2062: Add support for Postgres 15
    In Postgres 15, the `pg_class` table does not have `relhasoids`, so
    Partman's code from this repository does not work in Postgres 15.
    None of our tables were created WITH OIDS, according to the query
    shown here and grepping the source code, so just removing the part
    that checks for OIDS will work. (This code that I'm removing hasn't
    been completely removed in recent versions of Partman, but it is
    guarded by a version check.)
    
    The change to `journal.quote_column` reflects type changes from
    Postgres 11 to Postgres 15, but also works against Postgres 11 (as
    in, this package still builds fine against PG 11 with this change).
    
    The install script needs to grant permissions to the public schema
    in Postgres 15.
    
    An additional `Dockerfile-15` has been added to create a Postgres 15
    based image. The Travis configuration has been updated to build that
    image.
    
    New scripts have been added to recreate the modified functions in
    production:
    - `journal.quote_column`
    - `partman.create_partition_id`
    - `partman.create_partition_time`
    
    [so]: https://stackoverflow.com/questions/70257495/postgresql-how-can-i-check-if-table-was-created-with-oids

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 psql -U postgres -c 'create database registrydb' postgres
 psql -U postgres -c 'create role api login PASSWORD NULL' postgres > /dev/null
 psql -U postgres -c 'GRANT ALL ON DATABASE registrydb TO api' postgres
+psql -U postgres -c 'grant all on schema public to api' registrydb
 sem-apply --url postgresql://api@localhost/registrydb
